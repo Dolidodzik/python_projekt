86e794c3f797dbe254838bc2242bdabf7ab1b6a4
Dominik Lech <dominiklechweb@gmail.com>
2026-04-06 19:09:21 +0200

    templatka startowa backendu

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # python_projekt
 projekt na zaliczenie programowanie w języku python 
+
+# odpalanie backendu:
+0. przejscie do katalogu backend
+1. uruchomienie swojego srodowiska wirutalnego
+2. zainstalowanie zależności: pip install -r requirements.txt
+3. odpalenie projektu: uvicorn main:app --reload --host 0.0.0.0 --port 8000
---
+++ backend/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, UploadFile, File, Form
+from fastapi.responses import Response
+import pandas as pd
+import io
+
+app = FastAPI()
+
+
+@app.post("/test/csv")
+async def impute(
+    file: UploadFile = File(...), method: str = Form(...), columns: str = Form(None)
+):
+    contents = await file.read()
+    df = pd.read_csv(io.StringIO(contents.decode("utf-8")))
+
+    # tescik
+    if "Embarked" in df.columns:
+        df = df[df["Embarked"] == "Q"]
+
+    output = df.to_csv(index=False)
+    return Response(content=output, media_type="text/csv")
